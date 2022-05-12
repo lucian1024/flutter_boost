db3c1c9ef0dc8a3a0e275b4adff7b542bac47289
Lucian <liuxin_scut@163.com>
2022-05-12 18:39:23 +0800

    migrate to 2.10.0

--- android/src/main/java/com/idlefish/flutterboost/containers/FlutterBoostFragment.java
+++ android/src/main/java/com/idlefish/flutterboost/containers/FlutterBoostFragment.java
@@ -19,6 +19,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import io.flutter.Log;
+import io.flutter.embedding.android.ExclusiveAppComponent;
 import io.flutter.embedding.android.FlutterFragment;
 import io.flutter.embedding.android.FlutterTextureView;
 import io.flutter.embedding.android.FlutterView;
@@ -32,7 +33,7 @@ import static com.idlefish.flutterboost.containers.FlutterActivityLaunchConfigs.
 import static com.idlefish.flutterboost.containers.FlutterActivityLaunchConfigs.EXTRA_URL;
 import static com.idlefish.flutterboost.containers.FlutterActivityLaunchConfigs.EXTRA_URL_PARAM;
 
-public class FlutterBoostFragment extends FlutterFragment implements FlutterViewContainer {
+public class FlutterBoostFragment extends FlutterFragment implements FlutterViewContainer, ExclusiveAppComponent<Activity> {
     private static final String TAG = "FlutterBoostFragment";
     private static final boolean DEBUG = false;
     private final String who = UUID.randomUUID().toString();
@@ -214,6 +215,11 @@ public class FlutterBoostFragment extends FlutterFragment implements FlutterView
     }
 
     @Override
+    public Activity getAppComponent() {
+        return getActivity();
+    }
+
+    @Override
     public boolean shouldRestoreAndSaveState() {
       if (getArguments().containsKey(ARG_ENABLE_STATE_RESTORATION)) {
         return getArguments().getBoolean(ARG_ENABLE_STATE_RESTORATION);
@@ -314,7 +320,7 @@ public class FlutterBoostFragment extends FlutterFragment implements FlutterView
     private void performAttach() {
         if (!isAttached) {
             // Attach plugins to the activity.
-            getFlutterEngine().getActivityControlSurface().attachToActivity(getActivity(), getLifecycle());
+            getFlutterEngine().getActivityControlSurface().attachToActivity(this, getLifecycle());
 
             if (platformPlugin == null) {
                 platformPlugin = new PlatformPlugin(getActivity(), getFlutterEngine().getPlatformChannel());
