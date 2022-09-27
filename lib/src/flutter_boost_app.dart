c594b37857c965aeae851123470933950973076c
soon <62131847+hello-soon@users.noreply.github.com>
2022-09-27 10:41:31 +0800

    fix: support return null

--- lib/src/flutter_boost_app.dart
+++ lib/src/flutter_boost_app.dart
@@ -602,7 +602,7 @@ class FlutterBoostAppState extends State<FlutterBoostApp> {
   void _completePendingResultIfNeeded<T extends Object>(String? uniqueId,
       {T? result}) {
     if (uniqueId != null && _pendingResult.containsKey(uniqueId)) {
-      _pendingResult[uniqueId]!.complete(result ?? {});
+      _pendingResult[uniqueId]!.complete(result);
       _pendingResult.remove(uniqueId);
     }
   }
